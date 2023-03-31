25d4af6e1a67358be038eb3c90c1c3bc7b6ac60a
Henry-GeekOfTheNorth <tanruiheng52@gmail.com>
2023-03-31 13:39:03 -0400

    Added 8 new Marriotte hotels

--- SQL backend/Queries.sql
+++ SQL backend/Queries.sql
@@ -5,13 +5,46 @@ CREATE TABLE Hotel(
     category VARCHAR(100) NOT NULL,
     number_Of_Rooms INT NOT NULL,
     address VARCHAR(100) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
     PRIMARY KEY (Hotel_ID,Hotel_Chain_ID),
-    FOREIGN KEY(Hotel_Chain_ID) REFERENCES HotelChain(HotelChainID)
+    FOREIGN KEY(Hotel_Chain_ID) REFERENCES HotelChain(HotelChainID),
+    CHECK (category IN ('ONE','TWO','THREE','FOUR','FIVE','SIX'))
 );
 
-DROP TABLE Hotel;
+-- Populate hotel Mariott
 
+INSERT INTO hotel
+VALUES
+(11111,10001,'FOUR',1000,'250 René-Lévesque Boulevard West Montreal, Quebec H2Z 1Z8','admin@Marriott.com'),
+(11112,10001,'FOUR',1000,'2359 Banff Trail NW Calgary, Alberta T2M-4L2','admin@Marriott.com'),
+(11113,10001,'FOUR',1000,'151 Bass Pro Mills Drive Vaughan, Ontario L4K 0E6','admin@Marriott.com'),
+(11114,10001,'FIVE',1000,'13475 Central Avenue Surrey, British Columbia V3T 0L8','admin@Marriott.com'),
+(11115,10001,'FIVE',1000,'340 de la Gauchetiere O Montreal, Quebec H2Z0C3','admin@Marriott.com'),
+(11116,10001,'FIVE',1000,'1 Centennial Drive Kananaskis Village, Alberta T0L 2H0','admin@Marriott.com'),
+(11117,10001,'THREE',1000,'1709 Lower Water Street Halifax, Nova Scotia B3J 1S5','admin@Marriott.com'),
+(11118,10001,'THREE',1000,'184 Adolphus Street St Andrews, New Brunswick E5B 1T7','admin@Marriott.com');
+
+-- Populate hotel JinJiang
+
+
+
+
+-- Populate hotel Hilton 
+
+
+
+-- Populate hotel IHG
+
+
+
+
+-- Populate hotel Wyndham
+
+
+
+
+
+-- DROP TABLE Hotel; 
 
 -- Hotel chain
 CREATE TABLE HotelChain(
@@ -32,7 +65,7 @@ VALUES
 ('IHG',10004,7500,'111 IHG street','admin@IHG.com'),
 ('Wyndham',10005,7500,'111 Wyndham street','admin@Wyndham.com');
 
--- Drop table hotelChain;
+Drop table hotelChain;
 
 SELECT * FROM HotelChain;
 
@@ -60,11 +93,11 @@ CREATE TABLE Customer(
     Last_name VARCHAR(100) NOT NULL,
     Address VARCHAR(100) NOT NULL,
     SIN INT,
-    Date_Of_Registration VARCHAR(100),
+    Date_Of_Registration DATE,
     PRIMARY KEY (CustomerID,SIN)
 );
 
--- DROP TABLE Customer;
+DROP TABLE Customer;
 
 -- Rental 
 CREATE TABLE Rental(
@@ -73,8 +106,8 @@ CREATE TABLE Rental(
     Room_ID INT NOT NULL,
     Hotel_ID INT NOT NULL,
     Hotel_Chain_ID INT NOT NULL,
-    Start_Date VARCHAR(100),
-    End_Date VARCHAR(100),
+    Start_Date DATE,
+    End_Date DATE,
     Paid BOOL,
     FOREIGN KEY(Customer_ID) REFERENCES Customer(CustomerID),
     FOREIGN KEY(Room_ID) REFERENCES Room(Room_ID),
@@ -82,7 +115,7 @@ CREATE TABLE Rental(
     FOREIGN KEY(Hotel_Chain_ID) REFERENCES HotelChain(HotelChainID)
 );
 
-DROP TABLE Rental;
+-- DROP TABLE Rental;
 
 -- Employee
 
@@ -101,9 +134,12 @@ DROP TABLE Rental;
 -- You have to implement the views as SQL views. 
 -- View 1: the first view is the number of available rooms per area. 
 -- View 2: the second view is the capacity of all the rooms of a specific hotel.
--- Same Area = Same Address?
+-- Contains the total number of available rooms within the same city 
 -- How to update room number in hotel?
+
 CREATE VIEW Available_Rooms AS
 SELECT A.address,B.name,A.Hotel_ID,A.number_Of_Rooms,A.category
 FROM Hotel A,Hotelchain B
 WHERE A.Hotel_Chain_ID = B.HotelChainID;
+
+Drop view available_rooms;
