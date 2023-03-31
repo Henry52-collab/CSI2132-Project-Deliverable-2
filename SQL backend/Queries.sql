09152b5b7b4e5b7127b892e633abcb4f0e7d72c9
Henry-GeekOfTheNorth <tanruiheng52@gmail.com>
2023-03-31 02:35:20 -0400

    Created new tables and added the first view

--- SQL backend/Queries.sql
+++ SQL backend/Queries.sql
@@ -1,3 +1,19 @@
+-- Hotel
+CREATE TABLE Hotel(
+	Hotel_ID INT,
+    Hotel_Chain_ID INT,
+    category VARCHAR(100) NOT NULL,
+    number_Of_Rooms INT NOT NULL,
+    address VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    PRIMARY KEY (Hotel_ID,Hotel_Chain_ID),
+    FOREIGN KEY(Hotel_Chain_ID) REFERENCES HotelChain(HotelChainID)
+);
+
+DROP TABLE Hotel;
+
+
+-- Hotel chain
 CREATE TABLE HotelChain(
 	Name VARCHAR(10),
 	HotelChainID INT PRIMARY KEY,
@@ -7,6 +23,7 @@ CREATE TABLE HotelChain(
 	
 );
 
+-- Insert five hotel chains
 INSERT INTO HotelChain
 VALUES
 ('Marriott',10001,7500,'111 Marriott street','admin@Marriott.com'),
@@ -19,6 +36,74 @@ VALUES
 
 SELECT * FROM HotelChain;
 
+-- Room
+CREATE TABLE Room(
+	Room_ID INT,
+    Hotel_ID INT,
+    Hotel_ChainID INT,
+    Price NUMERIC(10,10),
+    Capacity INT NOT NULL,
+    view VARCHAR(100) NOT NULL,
+    extension BOOL NOT NULL,
+    PRIMARY KEY (Room_ID,Hotel_ID,Hotel_ChainID),
+    FOREIGN KEY(Hotel_ID) REFERENCES Hotel(Hotel_ID),
+    FOREIGN KEY (Hotel_ChainID) REFERENCES HotelChain(HotelChainID)
+);
+
+DROP TABLE Room;
+
+-- Customer
+CREATE TABLE Customer(
+	CustomerID INT,
+    First_name VARCHAR(100) NOT NULL,
+    Middle_name VARCHAR(100) NOT NULL,
+    Last_name VARCHAR(100) NOT NULL,
+    Address VARCHAR(100) NOT NULL,
+    SIN INT,
+    Date_Of_Registration VARCHAR(100),
+    PRIMARY KEY (CustomerID,SIN)
+);
+
+-- DROP TABLE Customer;
+
+-- Rental 
+CREATE TABLE Rental(
+	Rental_ID INT NOT NULL PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+    Room_ID INT NOT NULL,
+    Hotel_ID INT NOT NULL,
+    Hotel_Chain_ID INT NOT NULL,
+    Start_Date VARCHAR(100),
+    End_Date VARCHAR(100),
+    Paid BOOL,
+    FOREIGN KEY(Customer_ID) REFERENCES Customer(CustomerID),
+    FOREIGN KEY(Room_ID) REFERENCES Room(Room_ID),
+    FOREIGN KEY(Hotel_ID) REFERENCES Hotel(Hotel_ID),
+    FOREIGN KEY(Hotel_Chain_ID) REFERENCES HotelChain(HotelChainID)
+);
+
+DROP TABLE Rental;
+
+-- Employee
 
 
 
+
+-- Booking
+
+
+
+-- Manager
+
+
+
+-- The user should be able to see two specific Views.
+-- You have to implement the views as SQL views. 
+-- View 1: the first view is the number of available rooms per area. 
+-- View 2: the second view is the capacity of all the rooms of a specific hotel.
+-- Same Area = Same Address?
+-- How to update room number in hotel?
+CREATE VIEW Available_Rooms AS
+SELECT A.address,B.name,A.Hotel_ID,A.number_Of_Rooms,A.category
+FROM Hotel A,Hotelchain B
+WHERE A.Hotel_Chain_ID = B.HotelChainID;
