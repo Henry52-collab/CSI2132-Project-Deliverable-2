725973e1a6d7a5c78175cb246bddd2b6f05320f0
Henry-GeekOfTheNorth <tanruiheng52@gmail.com>
2023-04-04 14:33:58 -0400

    Populated 2/5 hotels with rooms, provided general sql template

--- SQL backend/Queries.sql
+++ SQL backend/Queries.sql
@@ -12,19 +12,28 @@ CREATE TABLE Hotel(
 );
 
 -- Populate hotel Mariott
-
 INSERT INTO hotel
 VALUES
-(11111,10001,'FOUR',1000,'250 René-Lévesque Boulevard West Montreal, Quebec H2Z 1Z8','admin@Marriott.com'),
-(11112,10001,'FOUR',1000,'2359 Banff Trail NW Calgary, Alberta T2M-4L2','admin@Marriott.com'),
-(11113,10001,'FOUR',1000,'151 Bass Pro Mills Drive Vaughan, Ontario L4K 0E6','admin@Marriott.com'),
-(11114,10001,'FIVE',1000,'13475 Central Avenue Surrey, British Columbia V3T 0L8','admin@Marriott.com'),
-(11115,10001,'FIVE',1000,'340 de la Gauchetiere O Montreal, Quebec H2Z0C3','admin@Marriott.com'),
-(11116,10001,'FIVE',1000,'1 Centennial Drive Kananaskis Village, Alberta T0L 2H0','admin@Marriott.com'),
-(11117,10001,'THREE',1000,'1709 Lower Water Street Halifax, Nova Scotia B3J 1S5','admin@Marriott.com'),
-(11118,10001,'THREE',1000,'184 Adolphus Street St Andrews, New Brunswick E5B 1T7','admin@Marriott.com');
+(11111,10001,'FOUR',1000,'250 René-Lévesque Boulevard West Montreal, Quebec,H2Z 1Z8','admin@Marriott.com'),
+(11112,10001,'FOUR',1000,'2359 Banff Trail NW Calgary, Alberta,T2M-4L2','admin@Marriott.com'),
+(11113,10001,'FOUR',1000,'151 Bass Pro Mills Drive Vaughan, Ontario,L4K 0E6','admin@Marriott.com'),
+(11114,10001,'FIVE',1000,'13475 Central Avenue Surrey, British Columbia,V3T 0L8','admin@Marriott.com'),
+(11115,10001,'FIVE',1000,'340 de la Gauchetiere O Montreal, Quebec,H2Z0C3','admin@Marriott.com'),
+(11116,10001,'FIVE',1000,'1 Centennial Drive Kananaskis Village, Alberta,T0L 2H0','admin@Marriott.com'),
+(11117,10001,'THREE',1000,'1709 Lower Water Street Halifax, Nova Scotia, B3J 1S5','admin@Marriott.com'),
+(11118,10001,'THREE',1000,'184 Adolphus Street St Andrews, New Brunswick,E5B 1T7','admin@Marriott.com');
 
 -- Populate hotel JinJiang
+INSERT INTO hotel
+value
+(12111,10002,'THREE',1000,'240 René-Lévesque Boulevard West Montreal, Quebec, H2Z 1Z8','admin@JinJiang.com'),
+(12112,10002,'THREE',1000,'2378 Banff Trail NW Calgary, Alberta, T2M-4L2','admin@JinJiang.com'),
+(12113,10002,'FOUR',1000,'2077 Bass Pro Mills Drive Vaughan, Ontario, L4K 0E6','admin@JinJiang.com'),
+(12114,10002,'FOUR',1000,'13476 Central Avenue Surrey, British Columbia, V3T 0L8','admin@JinJiang.com'),
+(12115,10002,'FOUR',1000,'341 de la Gauchetiere O Montreal, Quebec, H2Z0C3','admin@JinJiang.com'),
+(12116,10002,'FOUR',1000,'2 Centennial Drive Kananaskis Village, Alberta, T0L 2H0','admin@JinJiang.com'),
+(12117,10002,'TWO',1000,'1710 Lower Water Street Halifax, Nova Scotia, B3J 1S5','admin@JinJiang.com'),
+(12118,10002,'TWO',1000,'185 Adolphus Street St Andrews, New Brunswick, E5B 1T7','admin@JinJiang.com');
 
 
 
@@ -44,6 +53,9 @@ VALUES
 
 
 
+
+
+
 -- DROP TABLE Hotel; 
 
 -- Hotel chain
@@ -53,7 +65,6 @@ CREATE TABLE HotelChain(
 	numberOfHotels INT,
 	Address VARCHAR(30),
 	Email VARCHAR(30)
-	
 );
 
 -- Insert five hotel chains
@@ -74,16 +85,172 @@ CREATE TABLE Room(
 	Room_ID INT,
     Hotel_ID INT,
     Hotel_ChainID INT,
-    Price NUMERIC(10,10),
+    Price INT,
     Capacity INT NOT NULL,
     view VARCHAR(100) NOT NULL,
     extension BOOL NOT NULL,
+    availability BOOl NOT NULL,
     PRIMARY KEY (Room_ID,Hotel_ID,Hotel_ChainID),
     FOREIGN KEY(Hotel_ID) REFERENCES Hotel(Hotel_ID),
     FOREIGN KEY (Hotel_ChainID) REFERENCES HotelChain(HotelChainID)
 );
 
-DROP TABLE Room;
+-- Populate rooms
+
+-- Insert rooms into Marriott hotels
+-- 11111
+INSERT INTO Room
+VALUES
+(000,11111,10001,120,3,'sea',false,true),
+(001,11111,10001,130,2,'mountain',false,true),
+(002,11111,10001,150,1,'mountain',true,true),
+(003,11111,10001,120,3,'mountain',false,true),
+(004,11111,10001,130,2,'mountain',false,true);
+
+-- 11112
+INSERT INTO Room
+VALUES
+(005,11112,10001,120,3,'sea',true,true),
+(006,11112,10001,130,2,'mountain',false,true),
+(007,11112,10001,150,1,'sea',false,true),
+(008,11112,10001,120,3,'sea',false,true),
+(009,11112,10001,130,2,'mountain',false,true);
+
+-- 11113
+INSERT INTO Room
+VALUES
+(010,11113,10001,120,3,'sea',false,true),
+(011,11113,10001,130,2,'mountain',false,true),
+(012,11113,10001,150,1,'sea',false,true),
+(013,11113,10001,120,3,'sea',true,true),
+(014,11113,10001,130,2,'mountain',false,true);
+
+-- 11114
+INSERT INTO Room
+VALUES
+(015,11114,10001,120,3,'sea',true,true),
+(016,11114,10001,130,2,'mountain',true,true),
+(017,11114,10001,150,1,'sea',true,true),
+(018,11114,10001,120,3,'sea',true,true),
+(019,11114,10001,130,2,'mountain',false,true);
+
+-- 11115
+INSERT INTO Room
+VALUES
+(020,11114,10001,120,3,'sea',true,true),
+(021,11114,10001,130,2,'mountain',true,true),
+(022,11114,10001,150,1,'sea',true,true),
+(023,11114,10001,120,3,'sea',true,true),
+(024,11114,10001,130,2,'mountain',true,true);
+
+-- 11116
+INSERT INTO Room
+VALUES
+(025,11114,10001,120,3,'sea',false,true),
+(026,11114,10001,130,2,'sea',false,true),
+(027,11114,10001,150,1,'sea',true,true),
+(028,11114,10001,120,3,'sea',true,true),
+(029,11114,10001,130,2,'sea',true,true);
+
+-- 11117
+INSERT INTO Room
+VALUES
+(030,11114,10001,120,3,'moutain',false,true),
+(031,11114,10001,130,2,'moutain',false,true),
+(032,11114,10001,150,1,'moutain',true,true),
+(033,11114,10001,120,3,'moutain',true,true),
+(034,11114,10001,130,2,'moutain',true,true);
+
+-- 11118
+INSERT INTO Room
+VALUES
+(035,11114,10001,120,3,'moutain',false,true),
+(036,11114,10001,130,2,'moutain',false,true),
+(037,11114,10001,150,1,'moutain',false,true),
+(038,11114,10001,120,3,'moutain',false,true),
+(039,11114,10001,130,2,'moutain',false,true);
+
+-- Insert rooms into JingJiang hotels 
+-- 12111
+INSERT INTO Room
+VALUES
+(100,12111,10002,120,3,'sea',false,true),
+(101,12111,10002,130,2,'sea',false,true),
+(102,12111,10002,150,1,'sea',false,true),
+(103,12111,10002,120,3,'sea',false,true),
+(104,12111,10002,130,2,'sea',false,true);
+
+-- 12112
+INSERT INTO Room
+VALUES
+(105,12112,10002,120,3,'mountain',false,true),
+(106,12112,10002,130,2,'sea',true,true),
+(107,12112,10002,150,1,'mountain',false,true),
+(108,12112,10002,120,3,'sea',false,true),
+(109,12112,10002,130,2,'sea',false,true);
+
+-- 12113
+INSERT INTO Room
+VALUES
+(110,12113,10002,120,3,'mountain',false,true),
+(111,12113,10002,130,2,'sea',true,true),
+(112,12113,10002,150,1,'mountain',false,true),
+(113,12113,10002,120,3,'sea',false,true),
+(114,12113,10002,130,2,'mountain',true,true);
+
+-- 12114
+INSERT INTO Room
+VALUES
+(115,12114,10002,120,3,'mountain',false,true),
+(116,12114,10002,130,2,'mountain',false,true),
+(117,12114,10002,150,1,'mountain',false,true),
+(118,12114,10002,120,3,'sea',false,true),
+(119,12114,10002,130,2,'mountain',true,true);
+
+-- 12115
+INSERT INTO Room
+VALUES
+(120,12115,10002,120,3,'sea',true,true),
+(121,12115,10002,130,2,'mountain',true,true),
+(122,12115,10002,150,1,'mountain',false,true),
+(123,12115,10002,120,3,'sea',false,true),
+(124,12115,10002,130,2,'mountain',true,true);
+
+-- 12116
+INSERT INTO Room
+VALUES
+(125,12116,10002,120,3,'sea',false,true),
+(126,12116,10002,130,2,'sea',true,true),
+(127,12116,10002,150,1,'mountain',false,true),
+(128,12116,10002,120,3,'sea',false,true),
+(129,12116,10002,130,2,'mountain',true,true);
+
+-- 12117
+INSERT INTO Room
+VALUES
+(130,12117,10002,120,3,'sea',true,true),
+(131,12117,10002,130,2,'sea',true,true),
+(132,12117,10002,150,1,'sea',false,true),
+(133,12117,10002,120,3,'sea',false,true),
+(134,12117,10002,130,2,'mountain',false,true);
+
+-- 12118
+INSERT INTO Room
+VALUES
+(135,12117,10002,120,3,'sea',false,true),
+(136,12117,10002,130,2,'mountain',true,true),
+(137,12117,10002,150,1,'mountain',false,true),
+(138,12117,10002,120,3,'sea',false,true),
+(139,12117,10002,130,2,'mountain',false,true);
+
+
+-- Insert rooms into Hilton hotels
+
+-- Insert rooms into IHG hotels
+
+-- Insert rooms into Wyndham hotels
+
+-- DROP TABLE Room;
 
 -- Customer
 CREATE TABLE Customer(
@@ -137,9 +304,11 @@ CREATE TABLE Rental(
 -- Contains the total number of available rooms within the same city 
 -- How to update room number in hotel?
 
+SELECT substring(A.address,LOCATE(',',A.address),length(A.address)) FROM hotel A;
+
 CREATE VIEW Available_Rooms AS
-SELECT A.address,B.name,A.Hotel_ID,A.number_Of_Rooms,A.category
+SELECT A.address,B.name,SUM(A.number_Of_Rooms),A.category
 FROM Hotel A,Hotelchain B
-WHERE A.Hotel_Chain_ID = B.HotelChainID;
+WHERE A.Hotel_Chain_ID = B.HotelChainID AND A.address LIKE '%%';
 
 Drop view available_rooms;
